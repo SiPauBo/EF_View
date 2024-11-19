776477c4113cee410d2640bfd2d6d5f9ee0adbfe
ninagr09 <n.graf1@htlkrems.at>
2024-11-19 11:47:50 +0100

    update

---
+++ Inheritance_Library/Model/Migrations/20241119104256_initreate.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class initreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "PERSONS",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    FIRST_NAME = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    LAST_NAME = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DATE_OF_BIRTH = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PERSONS", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "AUTHORS",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Biography = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AUTHORS", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AUTHORS_PERSONS_Id",
+                        column: x => x.Id,
+                        principalTable: "PERSONS",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "CUSTOMERS",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    MembershipDate = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CUSTOMERS", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CUSTOMERS_PERSONS_Id",
+                        column: x => x.Id,
+                        principalTable: "PERSONS",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "LIBRARIANS",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    EmployeeId = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LIBRARIANS", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LIBRARIANS_PERSONS_Id",
+                        column: x => x.Id,
+                        principalTable: "PERSONS",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ITEMS",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    TITLE = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    AUTHOR = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PUBLISHED_DATE = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    ISBN = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    AuthorId = table.Column<int>(type: "int", nullable: false),
+                    ITEM_TYPE = table.Column<string>(type: "varchar(21)", maxLength: 21, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ITEMS", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ITEMS_AUTHORS_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "AUTHORS",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "BookDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    TotalCopies = table.Column<int>(type: "int", nullable: false),
+                    BorrowedCopies = table.Column<int>(type: "int", nullable: false),
+                    AvailableCopies = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BookDetails_ITEMS_Id",
+                        column: x => x.Id,
+                        principalTable: "ITEMS",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "BookManagement",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    LibrarianId = table.Column<int>(type: "int", nullable: false),
+                    LoanDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    ReturnLibrarianId = table.Column<int>(type: "int", nullable: true),
+                    ReturnDate = table.Column<DateTime>(type: "datetime(6)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookManagement", x => new { x.CustomerId, x.BookId, x.LibrarianId });
+                    table.ForeignKey(
+                        name: "FK_BookManagement_CUSTOMERS_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "CUSTOMERS",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookManagement_ITEMS_BookId",
+                        column: x => x.BookId,
+                        principalTable: "ITEMS",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookManagement_LIBRARIANS_LibrarianId",
+                        column: x => x.LibrarianId,
+                        principalTable: "LIBRARIANS",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookManagement_LIBRARIANS_ReturnLibrarianId",
+                        column: x => x.ReturnLibrarianId,
+                        principalTable: "LIBRARIANS",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookManagement_BookId",
+                table: "BookManagement",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookManagement_LibrarianId",
+                table: "BookManagement",
+                column: "LibrarianId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookManagement_ReturnLibrarianId",
+                table: "BookManagement",
+                column: "ReturnLibrarianId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ITEMS_AuthorId",
+                table: "ITEMS",
+                column: "AuthorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookDetails");
+
+            migrationBuilder.DropTable(
+                name: "BookManagement");
+
+            migrationBuilder.DropTable(
+                name: "CUSTOMERS");
+
+            migrationBuilder.DropTable(
+                name: "ITEMS");
+
+            migrationBuilder.DropTable(
+                name: "LIBRARIANS");
+
+            migrationBuilder.DropTable(
+                name: "AUTHORS");
+
+            migrationBuilder.DropTable(
+                name: "PERSONS");
+        }
+    }
+}
